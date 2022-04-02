237820279c9923100270ba4cb75c9f65d05d8db1
Matheus Portes <mportesif@gmail.com>
2022-04-02 19:44:14 -0300

    adicionada as unidades de medida e a funcionalidade de informar ao usuário o peso que ele deve ganhar ou perder para chegar ao peso normal

--- Program.cs
+++ Program.cs
@@ -17,27 +17,46 @@ namespace CalculadoraIMC
 
             double imc = Math.Round((peso / (altura * altura)), 2);
 
-            Console.WriteLine("\n{0}, sua altura é {1} e seu peso é {2}", nome, altura, peso);
+            Console.WriteLine("\n{0}, sua altura é {1}m e seu peso é {2}kg", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
             Console.Write("Classificação: ");
 
             if(imc < 18.5)
+            {
                 Console.WriteLine("Abaixo do Peso");
-
+            }
             else if(imc >= 18.5 && imc <= 24.9)
+            {
                 Console.WriteLine("Peso Normal");
-
-            else if(imc >= 25 && imc <= 29.9)
+            }
+            else if(imc >= 25.0 && imc <= 29.9)
+            {
                 Console.WriteLine("Sobrepeso");
-
-            else if(imc >= 30 && imc <= 34.9)
+            }
+            else if(imc >= 30.0 && imc <= 34.9)
+            {
                 Console.WriteLine("Obesidade Grau I");
-
-            else if(imc >= 35 && imc <= 39.9)
+            }
+            else if(imc >= 35.0 && imc <= 39.9)
+            {
                 Console.WriteLine("Obesidade Grau II");
-
+            }
             else
+            {
                 Console.WriteLine("Obesidade Grau III ");
+            }
+            
+            double pesoNormal;
+            if(imc < 18.5)
+            {
+                pesoNormal = 18.5 * altura * altura;
+                Console.WriteLine("Você precisa ganhar {0}kg para chegar ao peso normal.", Math.Round((pesoNormal - peso), 5));
+            }
+            else if(imc >= 25.0)
+            {
+                pesoNormal = 24.9 * altura * altura;
+                Console.WriteLine("Você precisa perder {0}kg para chegar ao peso normal.", Math.Round((peso - pesoNormal), 5));
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Calculadora IMC
 
-Aplicação console para cálculo do IMC.
+Aplicação console para cálculo do IMC, que permite avaliar se a pessoa está dentro do peso que é considerado ideal para a sua altura.
 
 ## Funcionalidades
 
 - Calcula o IMC
 - Informa a classificação do IMC
+- Informa o peso que o usuário deve ganhar ou perder para chegar ao peso normal
 
 ## Tecnologias utilizadas
 
-- dotnet 2.1
+- dotnet 6.0
 
 ## Como executar
 
